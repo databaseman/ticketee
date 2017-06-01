6e004b2add0957609faff47fc7232786c04b3e94
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-06-01 13:59:21 -0700

    Added permissions for editing tickets

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -10,8 +10,14 @@ class TicketPolicy < ApplicationPolicy
   end
 
   def create?
-    user.try(:admin?) || record.project.has_manager?(user) ||
-    record.project.has_editor?(user)
+    user.try(:admin?) || record.project.has_manager?(user) || record.project.has_editor?(user)
+  end
+
+  #The user is an admin.
+  #The user is a manager of the project.
+  #The user is an editor of the project, and the user is also the author of the ticket.
+  def update?
+    user.try(:admin?) || record.project.has_manager?(user) || (record.project.has_editor?(user) && record.author == user)
   end
 
 end
